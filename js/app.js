d7e29a30e6cb4c2f22744a9da36e118d3f775838
Mahin Khadir <mahinkhadir@Mahins-iMac.cust.communityfibre.co.uk>
2023-06-15 00:11:28 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+function openPopup(imgSrc) {
+    document.getElementById('popupImg').src = imgSrc;
+    document.querySelector('.popup').style.display = 'grid';
+}
+
+function closePopup() {
+    document.querySelector('.popup').style.display = 'none';
+
+
+
+
+}
+
+
+function openPopup(imgSrc) {
+    document.getElementById('popupImg2').src = imgSrc;
+    document.querySelector('.popup2').style.display = 'grid';
+}
+
+function closePopup() {
+    document.querySelector('.popup2').style.display = 'none';
+
+
+
+
+}
+
+function openPopup(imgSrc) {
+    document.getElementById('popupImg3').src = imgSrc;
+    document.querySelector('.popup3').style.display = 'grid';
+}
+
+function closePopup() {
+    document.querySelector('.popup3').style.display = 'none';
+
+
+
+
+}
